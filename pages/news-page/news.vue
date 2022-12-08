cb1babec790afc561169a44b3a7342fe8643ba7e
xiongwei_zhang <xiongwei_zhang@foxitsoftware.com>
2022-12-08 23:51:15 +0800

    开放主页

---
+++ pages/news-page/news.vue
@@ -0,0 +1,22 @@
+<template>
+	<view class="news-container">
+
+
+	news
+
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.news-container {}
+</style>
