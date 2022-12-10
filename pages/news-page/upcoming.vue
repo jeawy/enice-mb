73102f81c2e6540cc69168eb653cec9a2b5f661a
xiongwei_zhang <xiongwei_zhang@foxitsoftware.com>
2022-12-10 22:54:37 +0800

    新增页面

---
+++ pages/news-page/upcoming.vue
@@ -0,0 +1,73 @@
+<template>
+	<view class="upcoming">
+		<view class="date-tabs">
+			<view v-for="(item,idx) of tabs" :key="idx" @click="clickTab(idx)" :class="['date-tabs-item',active===idx?'type--active':'']">
+				{{item.name}}
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				active:0,
+				tabs: [{
+						name: "Recent"
+					}, {
+						name: "Dec"
+					},
+					{
+						name: "Jan"
+					},
+					{
+						name: "Feb"
+					},
+					{
+						name: "Mar"
+					},
+					{
+						name: "Apr"
+					},
+				]
+			};
+		},
+		methods:{
+			clickTab(idx){
+				this.active = idx
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	page{
+		background-color: #fff;
+	}
+	.upcoming {
+		padding: 0 40upx;
+		.date-tabs {
+			display: flex;
+			.date-tabs-item {
+				font-size: 20upx;
+				padding: 16upx 0;
+				border-radius: 30upx;
+				background: #FAFAFA;
+				color: #666666;
+				min-width: 72upx;
+				height: 36upx;
+				display: flex;
+				align-items: center;justify-content: center;
+				
+				&+.date-tabs-item {
+					margin-left: 20upx;
+				}
+				&.type--active{
+					color: #fff;
+					background: linear-gradient(180deg, #4B9A87 0.66%, #6ADEC3 100%);
+				}
+			}
+		}
+	}
+</style>
